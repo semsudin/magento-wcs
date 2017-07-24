de625c6bbdcd102f17ad8304d98c3a620847adce
jakubpolomsky <jakub.polomsky@partner.wirecard.com>
2017-07-24 10:23:26 +0200

    #15 update client library loader

--- app/code/community/Wirecard/CheckoutSeamless/Model/Autoloader.php
+++ app/code/community/Wirecard/CheckoutSeamless/Model/Autoloader.php
@@ -50,10 +50,8 @@ class Wirecard_CheckoutSeamless_Model_Autoloader extends Mage_Core_Model_Observe
         // rewrite class filename, avoid conflicts with installed old plugin, which resides under WirecardCEE
         if (preg_match('/^WirecardCEE_/', $class)) {
             if(defined('COMPILER_INCLUDE_PATH')) {
-                $class = str_replace('WirecardCEE', 'Wirecard_CheckoutSeamless', $class);
                 $classFile = COMPILER_INCLUDE_PATH . DIRECTORY_SEPARATOR . $class . '.php';
             } else {
-                $class = str_replace('WirecardCEE', 'Wirecard' . DIRECTORY_SEPARATOR . 'CheckoutSeamless', $class);
                 $classFile = str_replace(' ', DIRECTORY_SEPARATOR, ucwords(str_replace('_', ' ', $class))) . '.php';
             }
             include $classFile;
